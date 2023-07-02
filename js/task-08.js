47d82c60c7b9ee0d384a6469975777fb5f9e4ffa
Andrzej Kubiczek <80128407+andrzejkubiczek@users.noreply.github.com>
2023-07-02 22:35:58 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value && password.value) {
+    const userValues = {
+      email: email.value,
+      password: password.value,
+    };
+
+    console.log(userValues);
+    event.currentTarget.reset();
+  } else {
+    alert('Please complete all the fields!');
+  }
+});
