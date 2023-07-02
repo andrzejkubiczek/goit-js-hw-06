ecd83efbda4c5f0eeb0c8b331919574d1211d3ad
Andrzej Kubiczek <80128407+andrzejkubiczek@users.noreply.github.com>
2023-07-02 22:43:09 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeButton = document.querySelector('.change-color');
+const bodyElement = document.body;
+const colorValue = document.querySelector('.color');
+
+changeButton.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  bodyElement.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
+});
