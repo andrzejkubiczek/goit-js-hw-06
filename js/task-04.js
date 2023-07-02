da52530eba9c5c0151b183f8a0bffe7319ba111b
Andrzej Kubiczek <80128407+andrzejkubiczek@users.noreply.github.com>
2023-07-02 21:30:47 +0200

    task4

---
+++ js/task-04.js
@@ -0,0 +1,15 @@
+let counterValue = 0;
+
+const valueEl = document.querySelector('#value');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+decrementBtn.addEventListener('click', () => {
+  counterValue--;
+  valueEl.textContent = counterValue;
+});
+
+incrementBtn.addEventListener('click', () => {
+  counterValue++;
+  valueEl.textContent = counterValue;
+});
