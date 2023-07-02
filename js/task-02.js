04816d0479036f405ab3ae44a6596e5d41b3811e
Andrzej Kubiczek <80128407+andrzejkubiczek@users.noreply.github.com>
2023-07-02 15:58:42 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+ingredients.forEach(element => {
+  const ingredientsItem = document.createElement('li');
+  ingredientsItem.classList.add('item');
+  ingredientsItem.textContent = element;
+  ingredientsList.append(ingredientsItem);
+});
